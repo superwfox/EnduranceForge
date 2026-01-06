e32d6d52e932a5776e65eed79b97de56eb7516de
SUDARK <2963502563@qq.com>
2026-01-06 16:36:01 +0800

    Sudark

---
+++ src/main/java/sudark2/Sudark/enduranceForge/EnduranceForge.java
@@ -0,0 +1,17 @@
+package sudark2.Sudark.enduranceForge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnduranceForge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
